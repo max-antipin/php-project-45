ef20c7377f1cb0e75b91fdf3cb17ce74a65e3c04
Max Antipin <max.v.antipin@gmail.com>
2025-11-29 19:40:15 +0300

    Add brain-even app

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,10 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function run(): void
+function promptAndPrintName(): string
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?', marker:' ');
     line('Hello, %s!', $name);
+    return $name;
 }
